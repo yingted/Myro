02679585acf1fa6371052ba1e84508f69b9a0c46
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2006-08-24 19:05:21 +0000

    Scribbler initial files
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@2 ed25d823-a23d-414a-8cb8-cff62a94a292

---
+++ trunk/Scribbler/pinger.py
@@ -0,0 +1,46 @@
+import time
+import sys
+import logging
+import time
+import serial
+from numpy import *
+
+ser = serial.Serial(5, timeout=5)  
+ser.baudrate = 9600
+print ser
+
+ser.flushInput()
+ser.flushOutput()
+
+def test():
+    data = "a"
+    ser.write(data)
+    buf = ser.read(1)
+    if (buf != data):
+        print "ERROR: Mismatch"
+    buf = ser.read(1)    
+    if (buf != data):
+        print "ERROR: Mismatch"
+        
+
+if __name__ == '__main__':
+
+    try:
+        outfile = "home-scrib.txt"
+        if (len(sys.argv) == 2):
+            outfile = sys.argv[1]
+
+        print "About to start test."
+        a = []
+        for i in range(0,10000):
+            before = time.clock()
+            test()
+            a.append(time.clock() - before)
+
+        numpy_a = array(a)
+        numpy_a.tofile(outfile, sep='\n', format = "%e")
+        
+        print "Done!"
+    finally:
+        ser.close()
+
