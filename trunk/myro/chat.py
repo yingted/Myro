31d48b0b5bc8bdc9496b7a05e33f778def6e2c21
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2006-10-11 00:29:10 +0000

    Simplified the logging in/registering process
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@62 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/myro/chat.py
+++ trunk/myro/chat.py
@@ -4,37 +4,45 @@ Code to allow users to chat with each other.
 
 import xmpp, threading, time
 
+__version__ = "$Revision$"
+__author__  = "Doug Blank <dblank@cs.brynmawr.edu>"
+
 class Chat:
-    def __init__(self, name, password):
+    def __init__(self, name, password, server = None, debug = []):
+	""" Constructs a connection for communicating to the IM server. """
         self.lock = threading.Lock()
         self.messages = []
         # Start a thread up in here
         self.name = name
         self.password = password
-        self.masterServer = "mightymouse.brynmawr.edu"
-        self.masterJID = "test@" + self.masterServer
-        self.masterPassword = "test"
-        jid = xmpp.protocol.JID(self.masterJID)
-        self.client = xmpp.Client(jid.getDomain(),debug=[])
-        print "Making master connection to server..."
+	if server == None:
+	    self.server = "blog.roboteducation.org"
+	else:
+            self.server = server
+	self.debug = debug
+        self.client = xmpp.Client(self.server, debug=self.debug)
+        print "Making connection to server..."
         self.client.connect()
-        print "Authenticating master password..."
-        self.client.auth(jid.getNode(),self.masterPassword)
         print "Registering '%s'..." % self.name
         self.register(self.name, self.password)
-        self.client.disconnect()
         self.open()
 
     def register(self, name, password):
-        xmpp.features.register(self.client, self.masterServer,
+	""" Register a username/password. """
+        xmpp.features.register(self.client, self.server,
                                {"username": name,
                                 "password": password})
+
     def messageCB(self, conn, msg):
+	""" Message handling callback function. """
         self.lock.acquire()
         self.messages.append(msg)
         self.lock.release()
 
     def receive(self):
+	"""
+	Get all of the pending messages, and return them as a list.
+	"""
         self.client.Process(1) # this should be in a thread
         self.lock.acquire()
         retval = self.messages
@@ -43,30 +51,36 @@ class Chat:
         return retval
 
     def send(self, to, message):
-        self.client.send(xmpp.protocol.Message(to + "@" + self.masterServer,
-                                               message))
+	""" 
+	Send a message to a named recipient. They must be logged in.
+	"""
+        self.client.send(
+           xmpp.protocol.Message(to + "@" + self.server, message))
 
     def open(self):
-        jid = xmpp.protocol.JID(self.name + "@" + self.masterServer)
-        self.client = xmpp.Client(jid.getDomain(),debug=[])
-        print "Making new connection to server..."
-        self.client.connect()
-        print "Authenticating '%s' password..." % self.name
-        self.client.auth(jid.getNode(),self.password)
+	"""
+	Open a connection to the server.
+	"""
+        print "Authenticating password for '%s'..." % self.name
+        self.client.auth(self.name, self.password)
         print "Registering message handler..."
         self.client.RegisterHandler('message', self.messageCB) 
         self.client.sendInitPresence()
         self.send("", "2") # make this the only place I'm logged in        
         messages = self.receive()
         while len(messages) == 0:
-            print "waiting for response..."
+            print "   waiting for response..."
             time.sleep(1)
             messages = self.receive()
         print "Done!"
 
     def close(self):
+	"""
+	Close the connection to the server.
+	"""
         self.client.disconnect()
         print "Disconnected!"
 
     def __del__(self):
+	""" Close the connection on destruction. """
         self.close()
