21e6300e2b0dfb2927ecc62f1df12d8058b48ab9
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2007-01-24 13:29:40 +0000

    Added pause to install.bat
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@221 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/iso/Myro Install Files/install.bat
+++ trunk/iso/Myro Install Files/install.bat
@@ -12,3 +12,4 @@ copy /Y "Start Python.pyw"  "c:\Documents and Settings\%USERNAME%\Desktop\"
 copy /Y misc\config-extensions.def c:\Python24\Lib\idlelib\
 copy /Y misc\ScriptBinding.py c:\Python24\Lib\idlelib\
 copy /Y misc\graphics.py c:\Python24\Lib\site-packages\
+pause
