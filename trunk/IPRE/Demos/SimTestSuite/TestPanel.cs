ae43f3e54feb25431c3b8f6cbb77b221650aa000
t-richr <t-richr@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-05-14 17:11:05 +0000

    git-svn-id: http://svn.cs.brynmawr.edu/Myro@608 ed25d823-a23d-414a-8cb8-cff62a94a292

---
+++ trunk/IPRE/Demos/SimTestSuite/TestPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimTestSuite
+{
+    public partial class TestPanel : Form
+    {
+        public TestPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void TestPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
