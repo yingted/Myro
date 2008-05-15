6e4a1557d6a1b172830ca294ea6d21db6afccdc3
circuitb <circuitb@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-05-15 03:43:37 +0000

    Cleaned up command entry and history and fixed several cases where text could be edited inappropriately.
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@612 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/src/PythonShell.cs
+++ trunk/Pyjama/src/PythonShell.cs
@@ -26,46 +26,28 @@ class TextBufferOutputStream: Stream
     
     override public bool CanRead
     {
-        get
-        {
-            return false;
-        }
+        get { return false; }
     }
     
     override public bool CanSeek
     {
-        get
-        {
-            return false;
-        }
+        get { return false; }
     }
     
     override public bool CanWrite
     {
-        get
-        {
-            return true;
-        }
+        get { return true; }
     }
     
     override public long Length
     {
-        get
-        {
-            return 0;
-        }
+        get { return 0; }
     }
     
     override public long Position
     {
-        get
-        {
-            return 0;
-        }
-        
-        set
-        {
-        }
+        get { return 0; }
+        set {}
     }
     
     override public void Flush()
@@ -80,7 +62,9 @@ class TextBufferOutputStream: Stream
     override public void Write(byte[] data, int offset, int count)
     {
         string str = System.Text.Encoding.ASCII.GetString(data);
-        buffer.Text += str.Substring(0, count);
+        
+        TextIter iter = buffer.EndIter;
+        buffer.Insert(ref iter, str.Substring(0, count));
     }
     
     override public long Seek(long offset, SeekOrigin origin)
@@ -93,13 +77,183 @@ class TextBufferOutputStream: Stream
     }
 }
 
+class ShellSourceView: SourceView
+{
+    string prompt = ">>> ";
+    
+    // Where the command begins in the buffer (immediately after the prompt)
+    TextMark command_start;
+    
+    // Command history.
+    // history is never empty.  The last entry is the current command.
+    List<string> history = new List<string>();
+    int history_pos = 0;
+    
+    public delegate void ExecuteHandler(string line);
+    public event ExecuteHandler Execute;
+    
+    public ShellSourceView(SourceBuffer buf): base(buf)
+    {
+        KeyPressEvent += new KeyPressEventHandler(OnKey);
+        MoveCursor += new MoveCursorHandler(OnMove);
+    }
+    
+    public TextMark CommandStart
+    {
+        get { return command_start; }
+    }
+    
+    public void AppendText(string str)
+    {
+        TextIter iter = Buffer.EndIter;
+        Buffer.Insert(ref iter, str);
+    }
+    
+    protected override void OnBackspace()
+    {
+        TextIter start_iter = Buffer.GetIterAtMark(command_start);
+        int start_pos = start_iter.Offset;
+        if (Buffer.CursorPosition > start_pos)
+        {
+            base.OnBackspace();
+        }
+    }
+    
+    public void AddPrompt()
+    {
+        // Append the prompt to the buffer
+        AppendText(prompt);
+        Buffer.PlaceCursor(Buffer.EndIter);
+        
+        // Move the start-of-command mark
+        if (command_start == null)
+        {
+            command_start = Buffer.CreateMark(null, Buffer.EndIter, true);
+        } else {
+            Buffer.MoveMark(command_start, Buffer.EndIter);
+        }
+        
+        // Reset the history position
+        //FIXME - This is hazardous.
+        history.Add("");
+        history_pos = history.Count - 1;
+    }
+
+    // Returns true if the cursor is in a position where the command can be edited.
+    public bool CanEdit()
+    {
+        TextIter start_iter = Buffer.GetIterAtMark(command_start);
+        int start_pos = start_iter.Offset;
+        return Buffer.CursorPosition >= start_pos;
+    }
+
+    protected override bool OnButtonPressEvent(Gdk.EventButton e)
+    {
+        bool ret = base.OnButtonPressEvent(e);
+        Editable = CanEdit();
+        return ret;
+    }
+    
+    void OnMove(object obj, MoveCursorArgs args)
+    {
+        // args.RetVal=true doesn't stop the move, so we have to catch it afterwards
+        // and put the cursor back at the beginning of the line.
+        TextIter start_iter = Buffer.GetIterAtMark(command_start);
+        int start_pos = start_iter.Offset;
+        if (Buffer.CursorPosition < start_pos)
+        {
+            Buffer.PlaceCursor(start_iter);
+        }
+    }
+
+    // To get in the loop before the SourceView handles the keypress
+    // you need this directive:
+    [GLib.ConnectBefore]
+    void OnKey(object obj, KeyPressEventArgs args) 
+    {
+        // Had to cast as int on Windows for Gdk.Key and bit mask comparison
+        int key = (int)args.Event.Key;
+        
+        if (key == (int)Gdk.Key.Return || key == (int)Gdk.Key.KP_Enter) {
+            args.RetVal = true;
+            string line = Command;
+            AppendText("\n");
+            
+            // Ignore blank lines
+            if (line == "")
+            {
+                AddPrompt();
+                return;
+            }
+            
+            // FIXME - Add this command to history unless we were executing exactly the last command.
+            //  (So XYZ, (up), (up), (up) doesn't produce four copies of XYZ in the history.)
+            history[history.Count - 1] = line;
+            
+            Execute(line);
+            
+            AddPrompt();
+        } else if (key == (int)(Gdk.Key.Up)) {
+            args.RetVal = true;
+            if (history_pos > 0)
+            {
+                // Save the current command if it's the last one
+                if (history_pos == history.Count - 1)
+                {
+                    history[history.Count - 1] = Command;
+                }
+                
+                history_pos--;
+                Command = history[history_pos];
+            }
+        } else if (key == (int)(Gdk.Key.Down)) {
+            args.RetVal = true;
+            if (history_pos < (history.Count - 1))
+            {
+                history_pos++;
+                Command = history[history_pos];
+            }
+        } else if (key == (int)(Gdk.Key.Home)) {
+            //FIXME - Shift+HOME
+            args.RetVal = true;
+            Buffer.PlaceCursor(Buffer.GetIterAtMark(command_start));
+        } else {
+            // In case there's a key combination I forgot...
+            Editable = CanEdit();
+        }
+        
+        ScrollMarkOnscreen(Buffer.InsertMark);
+    }
+    
+    public string Command
+    {
+        get
+        {
+            TextIter start = Buffer.GetIterAtMark(command_start);
+            return Buffer.GetText(start, Buffer.EndIter, false);
+        }
+        
+        set
+        {
+            // Delete the current command
+            TextIter start = Buffer.GetIterAtMark(command_start);
+            TextIter end = Buffer.EndIter;
+            Buffer.Delete(ref start, ref end);
+            
+            // Add the new command
+            AppendText(value);
+            
+            // Put the cursor at the end of the line
+            Buffer.PlaceCursor(Buffer.EndIter);
+        }
+    }
+}
+
 public class PythonShell: PyjamaInterfaces.IShell
 {
     SourceBuffer buffer;
     SourceLanguage language;
-    SourceView source_view;
-    string prompt = ">>> ";
-    TextMark command_start;
+    ShellSourceView source_view;
     
 #if HOSTINGVER2
     private readonly ScriptEngine engine;
@@ -109,29 +263,25 @@ public class PythonShell: PyjamaInterfaces.IShell
     PythonEngine engine;
 #endif
     
-    List<string> history = new List<string>();
-    int history_pos = 0;
-    
     public PythonShell()
     {
         SourceLanguagesManager mgr = new SourceLanguagesManager();
-	language = mgr.GetLanguageFromMimeType("text/x-python");
+        language = mgr.GetLanguageFromMimeType("text/x-python");
         
         // Set up syntax highlighting
-	buffer = new SourceBuffer(language);
+        buffer = new SourceBuffer(language);
         buffer.Highlight = true;
-	source_view = new SourceView(buffer);
-	
+        source_view = new ShellSourceView(buffer);
+        
         // Change the font to fixed-width:
-	Pango.FontDescription font = new Pango.FontDescription();
-	font.Family = "Monospace";
-	source_view.ModifyFont(font);
-	
+        Pango.FontDescription font = new Pango.FontDescription();
+        font.Family = "Monospace";
+        source_view.ModifyFont(font);
+        
         // Event Handlers:
-	source_view.KeyPressEvent += new KeyPressEventHandler(OnSourceViewKey);
-        source_view.MoveCursor += new MoveCursorHandler(OnSourceViewMove);
-	source_view.WrapMode = Gtk.WrapMode.Word;
+        source_view.WrapMode = Gtk.WrapMode.Word;
         source_view.AutoIndent = true;
+        source_view.Execute += execute;
 
         TextBufferOutputStream output_stream = new TextBufferOutputStream(buffer);
         
@@ -153,31 +303,31 @@ public class PythonShell: PyjamaInterfaces.IShell
 #endif
         
         // Probably a more direct way to do these things:
-	command("import sys");
-	command("sys.path.append('.')");
-	command("sys.path.append('python')");
-	buffer.Text =  "# Python " + command("sys.version") + "\n";
-	buffer.Text += "# " + command("sys.copyright") + "\n";
-	command("del sys");
-	
-        add_prompt();
+        command("import sys");
+        command("sys.path.append('.')");
+        command("sys.path.append('python')");
+        buffer.Text += "# Python " + command("sys.version") + "\n" +
+                       "# " + command("sys.copyright") + "\n";
+        command("del sys");
+        
+        source_view.AddPrompt();
     }
-
+    
     // Evaluates an expression or executes a statement.
     // Call this for user command-line input when you don't know if
     // the string is an expression or statement.
     public string command(string line)
     {
-	string retval = "";
+        string retval = "";
 #if HOSTINGVER2
         try {
             object o = engine.CreateScriptSourceFromString(line, 
                                  SourceCodeKind.Expression).Execute(scope);
-	    if (o != null) {
-		retval = o.ToString();
-	    }
+            if (o != null) {
+                retval = o.ToString();
+            }
         } catch (Exception) {
-	    // FIXME: what is the IP2 version of PythonSyntaxErrorException?
+            // FIXME: what is the IP2 version of PythonSyntaxErrorException?
             // Not a valid expression, so try it as a statement
             try {
                 engine.CreateScriptSourceFromString(line, 
@@ -189,9 +339,9 @@ public class PythonShell: PyjamaInterfaces.IShell
 #elif HOSTINGVER1
         try {
             object o = engine.Evaluate(line);
-	    if (o != null) {
-		retval = o.ToString();
-	    }
+            if (o != null) {
+                retval = o.ToString();
+            }
         } catch (IronPython.Runtime.Exceptions.PythonSyntaxErrorException) {
             // Not a valid expression, so try it as a statement
             try {
@@ -204,157 +354,44 @@ public class PythonShell: PyjamaInterfaces.IShell
             retval = (e.Message + " at " + e.Source);
         }
 #endif
-	return retval;
+        return retval;
     }
 
-    void add_prompt()
+    void execute(string line)
     {
-        // Append the prompt to the buffer
-        buffer.Text += prompt;
-        buffer.PlaceCursor(buffer.EndIter);
-        
-        // Make a new start-of-command mark
-        if (command_start != null)
-        {
-            buffer.DeleteMark(command_start);
-        }
-        command_start = buffer.CreateMark(null, buffer.EndIter, true);
-        
-        // Reset the history position
-        history_pos = history.Count;
-    }
-
-    void execute()
-    {
-        TextIter start = buffer.GetIterAtMark(command_start);
-        string line = buffer.GetText(start, buffer.EndIter, false);
-        
-        buffer.Text += '\n';
-        
-        // Ignore blank lines
-        if (line == "")
-        {
-            add_prompt();
-            return;
-        }
-        
-        // FIXME - Add this command to history unless we were executing exactly the last command.
-        //  (So XYZ, (up), (up), (up) doesn't produce four copies of XYZ in the history.)
-        history.Add(line);
-        
-        /*
-        buffer.TagTable.Foreach(print_tag);
-        TextTag tag = buffer.TagTable.Lookup("Self");
-        if (tag == null)
-        {
-            Console.WriteLine("No blank tag");
-        } else {
-            buffer.ApplyTag(tag, buffer.StartIter, buffer.EndIter);
-            return;
-        }
-        */
-        
         // Evaluate or execute
-        buffer.Text += command(line);
-        
-        Console.WriteLine("History:");
-        foreach (string s in history)
-            Console.WriteLine(s);
-        Console.WriteLine("\n");
+        source_view.AppendText(command(line));
         
         // Don't give a newline unless we need one:
         int cursor_pos = buffer.CursorPosition;
         TextIter iter = buffer.GetIterAtOffset(cursor_pos);
         if (iter.CharsInLine != 0) {
-            buffer.Text += "\n";
-        }
-
-        add_prompt();
-    }
-
-    void OnSourceViewMove(object obj, MoveCursorArgs args)
-    {
-        Console.WriteLine("move");
-        // args.RetVal=true doesn't stop the move, so we have to catch it afterwards
-        // and put the cursor back at the beginning of the line.
-        TextIter start_iter = buffer.GetIterAtMark(command_start);
-        int start_pos = start_iter.Offset;
-        if (buffer.CursorPosition < start_pos)
-        {
-            buffer.PlaceCursor(start_iter);
+            source_view.AppendText("\n");
         }
     }
 
-    // To get in the loop before the SourceView handles the keypress
-    // you need this directive:
-    [GLib.ConnectBefore]
-    void OnSourceViewKey(object obj, KeyPressEventArgs args) 
-    {
-        // Had to cast as int on Windows for Gdk.Key and bit mask comparison
-        int key = (int)args.Event.Key;
-        
-        //FIXME - Left, right, backspace
-        
-	if (key == (int)(Gdk.Key.Return)) {
-            execute();
-	    args.RetVal = true;
-        } else if (key == (int)(Gdk.Key.Up)) {
-            if (history_pos > 0)
-            {
-                history_pos--;
-                SetCommand(history[history_pos]);
-            }
-            args.RetVal = true;
-        } else if (key == (int)(Gdk.Key.Down)) {
-            if (history_pos < (history.Count - 1))
-            {
-                history_pos++;
-                SetCommand(history[history_pos]);
-            }
-            args.RetVal = true;
-	} else if (key == (int)(Gdk.Key.Home)) {
-	    buffer.PlaceCursor(buffer.GetIterAtMark(command_start));
-	    args.RetVal = true;
-	}
-	source_view.ScrollMarkOnscreen(buffer.InsertMark);
-    }
-    
-    // Replaces the current command.
-    // Used for command history.
-    public void SetCommand(string line)
-    {
-        // Delete the current command
-        TextIter start = buffer.GetIterAtMark(command_start);
-        TextIter end = buffer.EndIter;
-        buffer.Delete(ref start, ref end);
-        
-        // Add the new command
-        end = buffer.EndIter;
-        buffer.Insert(ref end, line);
-    }
-    
     public void ExecuteFile(string filename)
     {
-	// FIXME: import file
+        // FIXME: import file
     }
 
     public void EvaluateExp(string expression)
     {
-	// FIXME: evaluate exp in env
+        // FIXME: evaluate exp in env
     }
 
     public void Restart()
     {
-	// Fixme: restart the shell (with new env)
+        // Fixme: restart the shell (with new env)
     }
 
     public void Quit()
     {
-	// FIXME: close the shell
+        // FIXME: close the shell
     }
 
     public Widget GetView()
     {
-	return (Widget) source_view;
+        return (Widget) source_view;
     }
 }
