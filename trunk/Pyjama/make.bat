22b2703fa7807a8341f2f3e05b5d07f53677a626
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-05-21 18:23:25 +0000

    Changes to make it make under Vista
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@634 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/make.bat
+++ trunk/Pyjama/make.bat
@@ -12,5 +12,11 @@ REM mcs -pkg:gtk-sharp-2.0 -pkg:gtksourceview-sharp-2.0 Pyjama.cs -r:PyjamaInter
 
 REM Had to copy gtksourceview-sharp.dll and gtk-sharp.dll to this dir
 REM Had to set MONO_PATH to include lib:
-gmcs -d:HOSTINGVER2 -recurse:src/*.cs -lib:./lib -r:gtk-sharp.dll -r:gtksourceview-sharp -r:Mono.Posix.dll -r:glib-sharp.dll -r:gdk-sharp.dll -r:pango-sharp.dll -r:IronPython.dll -r:Microsoft.Scripting.dll -r:Microsoft.Scripting.Core.dll -out:pyjama.exe 
+REM set MONO_PATH="c:\program files\Mono-1.9.1\lib"
+REM gmcs -d:HOSTINGVER2 -recurse:src/*.cs -lib:"c:\program files\Mono-1.9.1\lib\" -lib:. -pkg:gtk-sharp-2.0 -pkg:gtksourceview-sharp-2.0 -r:Mono.Posix.dll -pkg:glade-sharp-2.0 -r:gtk-sharp.dll -r:glib-sharp.dll -r:gdk-sharp.dll -r:pango-sharp.dll -r:IronPython.dll -r:Microsoft.Scripting.dll -r:Microsoft.Scripting.Core.dll -out:pyjama.exe 
+
+set PKG_CONFIG_PATH="c:\Program Files\Mono-1.9.1\lib\pkgconfig"
+set MONO_PATH="c:\program files\Mono-1.9.1\lib"
+gmcs -d:HOSTINGVER2 -recurse:src/*.cs -r:gtksourceview-sharp.dll -pkg:gtk-sharp-2.0 -r:glade-sharp.dll -r:Mono.Posix.dll -r:IronPython.dll -r:Microsoft.Scripting.dll -r:Microsoft.Scripting.Core.dll -out:pyjama.exe 
+
 
