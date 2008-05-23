dce5460864ca7abb953b851b7f6b65a759e38e7a
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-05-23 10:46:50 +0000

    Added notes on testme for win
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@639 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/make.bat
+++ trunk/Pyjama/make.bat
@@ -15,6 +15,12 @@ REM Had to set MONO_PATH to include lib:
 REM set MONO_PATH="c:\program files\Mono-1.9.1\lib"
 REM gmcs -d:HOSTINGVER2 -recurse:src/*.cs -lib:"c:\program files\Mono-1.9.1\lib\" -lib:. -pkg:gtk-sharp-2.0 -pkg:gtksourceview-sharp-2.0 -r:Mono.Posix.dll -pkg:glade-sharp-2.0 -r:gtk-sharp.dll -r:glib-sharp.dll -r:gdk-sharp.dll -r:pango-sharp.dll -r:IronPython.dll -r:Microsoft.Scripting.dll -r:Microsoft.Scripting.Core.dll -out:pyjama.exe 
 
+REM gmcs -target:library src/Testme.cs -out:Testme.dll
+REM namespace Testme {
+REM    class Doit {
+REM    }
+REM }
+
 set PKG_CONFIG_PATH="c:\Program Files\Mono-1.9.1\lib\pkgconfig"
 set MONO_PATH="c:\program files\Mono-1.9.1\lib"
 gmcs -d:HOSTINGVER2 -recurse:src/*.cs -r:gtksourceview-sharp.dll -pkg:gtk-sharp-2.0 -r:glade-sharp.dll -r:Mono.Posix.dll -r:IronPython.dll -r:Microsoft.Scripting.dll -r:Microsoft.Scripting.Core.dll -out:pyjama.exe 
