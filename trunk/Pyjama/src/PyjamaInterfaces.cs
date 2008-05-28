ae80250010801ba728137c919ffc08ffe65de59d
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-05-28 01:37:44 +0000

    Added copyright and license info to each file
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@651 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/src/PyjamaInterfaces.cs
+++ trunk/Pyjama/src/PyjamaInterfaces.cs
@@ -1,3 +1,19 @@
+/*********************************************************************
+ *
+ * Copyright (c) 2008 Douglas S. Blank
+ *
+ * This source code is subject to terms and conditions of the
+ * Microsoft Public License. A copy of the license can be found in the
+ * License.html file at the root of this distribution. If you cannot
+ * locate the Microsoft Public License, please send an email to
+ * dlr@microsoft.com. By using this source code in any fashion, you
+ * are agreeing to be bound by the terms of the Microsoft Public
+ * License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ *********************************************************************/
+
 using Gtk;
 
 namespace PyjamaInterfaces
