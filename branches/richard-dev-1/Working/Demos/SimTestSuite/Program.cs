d2110a60edd658287e6ba28ab477d42c3ca8f38d
t-richr <t-richr@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-06-03 23:48:00 +0000

    made a copy
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@699 ed25d823-a23d-414a-8cb8-cff62a94a292

---
+++ branches/richard-dev-1/Working/Demos/SimTestSuite/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Dss.Hosting;
+
+using IPREFoundationClasses;
+
+namespace SimTestSuite
+{
+    class SimTestSuite
+    {
+        ScribblerBrain scribbler;
+        TestPanel panel;
+
+        public void start()
+        {
+            Console.WriteLine("Starting DSS environment...");
+            DssEnvironment.Initialize(50000, 50001);
+            AdapterBank adapters = new AdapterBank("Config\\Scribbler.config.xml", true);
+
+            //Console.WriteLine("Opening Scribbler...");
+            //scribbler = new ScribblerBrain("Config\\IPRE.Scribbler.standard.config.xml");
+            //Console.WriteLine("Scribbler is ready!");
+
+            //new Thread(new ThreadStart(bumperLoop)).Start();
+            //new Thread(new ThreadStart(driveLoop)).Start();
+
+            startGUI();
+        }
+
+        void bumperLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("[ " + scribbler.getContact(0) + ", " + scribbler.getContact(1) + " ]");
+                Thread.Sleep(500);
+            }
+        }
+
+        void driveLoop()
+        {
+            scribbler.ForwardFor(1.0f, 10.0f);
+        }
+
+        void startGUI()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            panel = new TestPanel(scribbler);
+            Application.Run(panel);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimTestSuite test = new SimTestSuite();
+            test.start();
+        }
+    }
+}
