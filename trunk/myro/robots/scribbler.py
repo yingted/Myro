92ed51447c02d9d4ea08250594586699f6b0cb71
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-06-10 17:46:06 +0000

    Version 2.8.0 with jpeg-fast
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@729 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/myro/robots/scribbler.py
+++ trunk/myro/robots/scribbler.py
@@ -684,7 +684,7 @@ class Scribbler(Robot):
             self.lock.release()
         return jpeg
         
-    def takePicture(self, mode="color"):
+    def takePicture(self, mode="jpeg-fast"):
         width = 256
         height = 192
         p = Picture()
