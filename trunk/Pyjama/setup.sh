737b0515eca27048f5336b88c0b5eab8e86f32bf
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-07-16 00:37:00 +0000

    a working, minimum setup
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@803 ed25d823-a23d-414a-8cb8-cff62a94a292

---
+++ trunk/Pyjama/setup.sh
@@ -0,0 +1,5 @@
+export MONO_PATH="/opt/mono-1.9.1/lib/mono:DLLs"
+export LD_LIBRARY_PATH="/opt/mono-1.9.1/lib"
+export PATH="/opt/mono-1.9.1/bin:$PATH"
+export PKG_CONFIG_PATH="/opt/mono-1.9.1/share/pkgconfig:/opt/mono-1.9.1/lib/pkgconfig:$PKG_CONFIG_PATH"
+
