25815312541c8b451b69430540bedc0e52c3fa04
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2008-10-10 23:25:00 +0000

    Clean and successfully recompiles
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@1129 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/IronEditor/src/CopyLanguageConfig.bat
+++
@@ -1,4 +0,0 @@
-copy LanguageConfig\*.xml ..\Bin\Debug\Config\
-copy LanguageBinaries\IronPython\*.* ..\Bin\Debug\LanguageBinaries\IronPython\
-copy LanguageBinaries\IronRuby\*.* ..\Bin\Debug\LanguageBinaries\IronRuby\
-pause
