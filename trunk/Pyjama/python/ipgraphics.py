eee0145c201caecdd587dd24420c148f62019383
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2009-05-08 07:02:53 +0000

    graphics working, somewhat
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@1291 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/python/ipgraphics.py
+++ trunk/Pyjama/python/ipgraphics.py
@@ -92,7 +92,7 @@ def _ip_thread():
         _root = Form(Size = Drawing.Size(0,0))  # Create the dummy control, and show then hide it to get Windows Forms to initialize it.
         _root.Show()
         _root.Hide()
-        _are.Set()                              # Signal that the thread running _ip_thread is ready for the main thread to send input to it.
+        #_are.Set()                              # Signal that the thread running _ip_thread is ready for the main thread to send input to it.
         #Application.Run()
     finally:
         pass
@@ -107,12 +107,13 @@ def _ip_shutdown():
     _root.Invoke(CallTarget0(Application.Exit))
 
 # Interpeter exit should clean up and terminate thread.
-sys.exitfunc = _ip_shutdown
+#sys.exitfunc = _ip_shutdown
 
 # Start the thread.
-_are = AutoResetEvent(False)
-Thread(ThreadStart(_ip_thread)).Start()
-_are.WaitOne()
+#_are = AutoResetEvent(False)
+#Thread(ThreadStart(_ip_thread)).Start()
+#_are.WaitOne()
+_ip_thread()
 
 # ----------------------------------------
 class GraphWin(Form):
@@ -1059,46 +1060,46 @@ Pixmap = Image
 def test():
     win = GraphWin()
     win.setCoords(0,0,10,10)
-    t = Text(Point(5,5), "Centered Text")
-    t.draw(win)
-    p = Polygon(Point(1,1), Point(5,3), Point(2,7))
-    p.draw(win)
-    e = Entry(Point(5,6), 10)
-    e.draw(win)
-    win.getMouse()
-    p.setFill("red")
-    p.setOutline("blue")
-    p.setWidth(2)
-    s = ""
-    for pt in p.getPoints():
-        s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
-    t.setText(e.getText())
-    e.setFill("green")
-    e.setText("Spam!")
-    e.move(2,0)
-    win.getMouse()
-    p.move(2,3)
-    s = ""
-    for pt in p.getPoints():
-        s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
-    t.setText(s)
-    win.getMouse()
-    p.undraw()
-    e.undraw()
-    t.setStyle("bold")
-    win.getMouse()
-    t.setStyle("normal")
-    win.getMouse()
-    t.setStyle("italic")
-    win.getMouse()
-    t.setStyle("bold italic")
-    win.getMouse()
-    t.setSize(14)
-    win.getMouse()
-    t.setFace("arial")
-    t.setSize(20)
-    win.getMouse()
-    win.close()
+#     t = Text(Point(5,5), "Centered Text")
+#     t.draw(win)
+#     p = Polygon(Point(1,1), Point(5,3), Point(2,7))
+#     p.draw(win)
+#     e = Entry(Point(5,6), 10)
+#     e.draw(win)
+#     #win.getMouse()
+#     p.setFill("red")
+#     p.setOutline("blue")
+#     p.setWidth(2)
+#     s = ""
+#     for pt in p.getPoints():
+#         s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
+#     t.setText(e.getText())
+#     e.setFill("green")
+#     e.setText("Spam!")
+#     e.move(2,0)
+#     #win.getMouse()
+#     p.move(2,3)
+#     s = ""
+#     for pt in p.getPoints():
+#         s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
+#     t.setText(s)
+#     #win.getMouse()
+#     p.undraw()
+#     e.undraw()
+#     t.setStyle("bold")
+#     #win.getMouse()
+#     t.setStyle("normal")
+#     #win.getMouse()
+#     t.setStyle("italic")
+#     #win.getMouse()
+#     t.setStyle("bold italic")
+#     #win.getMouse()
+#     t.setSize(14)
+#     #win.getMouse()
+#     t.setFace("arial")
+#     t.setSize(20)
+#     #win.getMouse()
+#     win.close()
 
 # -----------------------------------------------
 #if __name__ == "__main__":
