8c9b060d7ba5910a1cecea92e9b5c75b0ee43e31
dblank <dblank@ed25d823-a23d-414a-8cb8-cff62a94a292>
2009-05-25 23:56:24 +0000

    colorizing!
    git-svn-id: http://svn.cs.brynmawr.edu/Myro@1347 ed25d823-a23d-414a-8cb8-cff62a94a292

--- trunk/Pyjama/src/Controls/Document.cs
+++ trunk/Pyjama/src/Controls/Document.cs
@@ -1,54 +1,60 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Pyjama
 {
 
-  public class MyRichTextBox: RichTextBox {
-    // Document Typing/Display Widget
-	protected override void OnKeyDown(KeyEventArgs e)
-	{
-	  //System.Console.WriteLine("OnKeyDown?");
-	  if (e.KeyData == Keys.Tab)
-	    {
-	      //System.Console.WriteLine("OnKeyDown!");
-	      this.SelectedText = "    ";                
-	      e.Handled = true;
-	    }
-	  else
-	    {
-	      base.OnKeyDown(e);
-	    }
-	}
-	
-	/*
-	protected override bool IsInputKey(Keys keyData)
-	{
-	  // To have [TAB, Return, Escape] keys raise the KeyDown
-	  // event, you must override the IsInputKey method in each
-	  // control on your form. The code for the override of the
-	  // IsInputKey would need to determine if one of the special
-	  // keys is pressed and return a value of true.
-	  System.Console.WriteLine("IsInputKey?");
-	  if (keyData == Keys.Tab)
-	    {
-	      System.Console.WriteLine("IsInputKey!");
-	      return true;
-	    }
-	  else
-	    {
-	      return base.IsInputKey(keyData);
-	    }
-	}
-	*/
-  }
+    public class MyRichTextBox : RichTextBox
+    {
+        // Document Typing/Display Widget
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            //System.Console.WriteLine("OnKeyDown?");
+            if (e.KeyData == Keys.Tab)
+            {
+                //System.Console.WriteLine("OnKeyDown!");
+                this.SelectedText = "    ";
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnKeyDown(e);
+            }
+        }
+
+        /*
+        protected override bool IsInputKey(Keys keyData)
+        {
+          // To have [TAB, Return, Escape] keys raise the KeyDown
+          // event, you must override the IsInputKey method in each
+          // control on your form. The code for the override of the
+          // IsInputKey would need to determine if one of the special
+          // keys is pressed and return a value of true.
+          System.Console.WriteLine("IsInputKey?");
+          if (keyData == Keys.Tab)
+            {
+              System.Console.WriteLine("IsInputKey!");
+              return true;
+            }
+          else
+            {
+              return base.IsInputKey(keyData);
+            }
+        }
+        */
+    }
 
     public partial class Document : UserControl, IDocument
     {
         public new event Document.TextChangedHandler TextChanged;
         private IMainForm MainForm;
         private RichTextBox textBox;
+        private Dictionary<string,Color> colors = new Dictionary<string,Color>();
+        private Dictionary<string,Font> fonts = new Dictionary<string,Font>();
+        public String[] keywords;
 
         public delegate void TextChangedHandler(object sender, EventArgs e);
 
@@ -67,6 +73,22 @@ namespace Pyjama
             textBox.TextChanged += textbox_TextChanged;
             textBox.Font = new Font("Courier New", 10);
             textBox.WordWrap = false;
+            // Resue fonts on formatting:
+            colors.Add("default", Color.Black);
+            fonts.Add("default", new Font("Courier New", 10, FontStyle.Regular));
+            colors.Add("comment", Color.LightGreen);
+            fonts.Add("comment", new Font("Courier New", 10, FontStyle.Regular));
+            colors.Add("keyword", Color.Blue);
+            fonts.Add("keyword", new Font("Courier New", 10, FontStyle.Bold));
+            keywords = new string[] { "and", "del", "for", "is", "raise",
+                                      "assert", "elif", "from", "lambda", "return",
+                                      "break", "else", "global", "not", "try",
+                                      "class", "except", "if", "or", "while",
+                                      "continue", "exec", "import", "pass", "yield",
+                                      "def", "finally", "in", "print",
+                                      "as", "with", "None", "True", "False"};
+
+            // -----------------------------
             Controls.Add(textBox);
         }
 
@@ -74,8 +96,8 @@ namespace Pyjama
         {
             // Do things when a key goes down  
             // FIXME: this can be wrong when selecting
-	        //System.Console.WriteLine("textBox_KeyUp: {0} {1} handled: {2}", 
-			//	     e.KeyCode, e.Control, e.Handled);
+            //System.Console.WriteLine("textBox_KeyUp: {0} {1} handled: {2}", 
+            //	     e.KeyCode, e.Control, e.Handled);
             int col = (textBox.SelectionStart - textBox.GetFirstCharIndexOfCurrentLine() + 1);
             int line = (textBox.GetLineFromCharIndex(textBox.SelectionStart) + 1);
             MainForm.UpdateGUI(col, line);
@@ -83,13 +105,13 @@ namespace Pyjama
 
         void textBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-	        //System.Console.WriteLine("Document.textBox_KeyPress: {0}, handled: {1}", 
-			//	     (int)e.KeyChar,
-			//	     e.Handled);
+            //System.Console.WriteLine("Document.textBox_KeyPress: {0}, handled: {1}", 
+            //	     (int)e.KeyChar,
+            //	     e.Handled);
             // Add ability to handle newlines after line ending with colon
             if (e.KeyChar == '\t') // tab
             {
-	            //System.Console.WriteLine("tab!");
+                //System.Console.WriteLine("tab!");
                 //int start = textBox.SelectionStart;
                 //textBox.Text = textBox.Text.Insert(start, "    ");
                 //textBox.SelectionStart = start + 4;
@@ -97,15 +119,132 @@ namespace Pyjama
             }
         }
 
+        void Parse(string text)
+        {
+            // Foreach line in input,
+            // identify key words and format them when adding to the rich text box.
+            Regex r = new Regex("\\n");
+            String[] lines = r.Split(text);
+            foreach (string l in lines)
+            {
+                ParseLine(l);
+            }
+        }
+
+        void ParseLine(string line)
+        {
+            RichTextBox m_rtb = textBox;
+
+            Regex r = new Regex("([ \\t{}();])");
+            String[] tokens = r.Split(line);
+            foreach (string token in tokens)
+            {
+                // Set the token's default color and font.
+                m_rtb.SelectionColor = colors["default"];
+                m_rtb.SelectionFont = fonts["default"];
+                // Check for a comment.
+                if (token.StartsWith("#"))
+                {
+                    // Find the start of the comment and then extract the whole comment.
+                    int index = line.IndexOf("#");
+                    string comment = line.Substring(index, line.Length - index);
+                    m_rtb.SelectionColor = colors["comment"];
+                    m_rtb.SelectionFont = fonts["comment"];
+                    m_rtb.SelectedText = comment;
+                    break;
+                }
+                // Check whether the token is a keyword. 
+                for (int i = 0; i < keywords.Length; i++)
+                {
+                    if (keywords[i] == token)
+                    {
+                        // Apply alternative color and font to highlight keyword.
+                        m_rtb.SelectionColor = colors["keyword"];
+                        m_rtb.SelectionFont = fonts["keyword"];
+                        break;
+                    }
+                }
+                m_rtb.SelectedText = token;
+            }
+            m_rtb.SelectedText = "\n";
+        } 
+
         void textbox_TextChanged(object sender, EventArgs e)
         {
             if (TextChanged != null)
                 TextChanged(this, e);
+            RichTextBox m_rtb = textBox;
+            // Calculate the starting position of the current line.
+            int start = 0, end = 0;
+            for (start = m_rtb.SelectionStart - 1; start > 0; start--)
+            {
+                if (m_rtb.Text[start] == '\n') { start++; break; }
+            }
+            // Calculate the end position of the current line.
+            for (end = m_rtb.SelectionStart; end < m_rtb.Text.Length; end++)
+            {
+                if (m_rtb.Text[end] == '\n') break;
+            }
+            // Extract the current line that is being edited.
+            String line = m_rtb.Text.Substring(start, end - start);
+            // Backup the users current selection point.
+            int selectionStart = m_rtb.SelectionStart;
+            int selectionLength = m_rtb.SelectionLength;
+            // Split the line into tokens.
+            Regex r = new Regex("([ \\t{}();])");
+            string[] tokens = r.Split(line);
+            int index = start;
+            foreach (string token in tokens)
+            {
+                // Set the token's default color and font.
+                m_rtb.SelectionStart = index;
+                m_rtb.SelectionLength = token.Length;
+                m_rtb.SelectionColor = colors["default"];
+                m_rtb.SelectionFont = fonts["default"];
+                // Check for a comment.
+                if (token.StartsWith("#"))
+                {
+                    // Find the start of the comment and then extract the whole comment.
+                    int length = line.Length - (index - start);
+                    string commentText = m_rtb.Text.Substring(index, length);
+                    m_rtb.SelectionStart = index;
+                    m_rtb.SelectionLength = length;
+                    m_rtb.SelectionColor = colors["comment"];
+                    m_rtb.SelectionFont = fonts["comment"];
+                    break;
+                }
+                else if (token.StartsWith("\""))
+                {
+                    // Find the start of the comment and then extract the whole comment.
+                    int length = line.Length - (index - start);
+                    string commentText = m_rtb.Text.Substring(index, length);
+                    m_rtb.SelectionStart = index;
+                    m_rtb.SelectionLength = length;
+                    m_rtb.SelectionColor = colors["comment"];
+                    m_rtb.SelectionFont = fonts["comment"];
+                    break;
+                }
+                // Check whether the token is a keyword. 
+                for (int i = 0; i < keywords.Length; i++)
+                {
+                    if (token == keywords[i])
+                    {
+                        // Apply alternative color and font to highlight keyword.        
+                        m_rtb.SelectionColor = colors["keyword"];
+                        m_rtb.SelectionFont = fonts["keyword"];
+                        break;
+                    }
+                }
+                index += token.Length;
+            }
+            // Restore the users current selection point.    
+            m_rtb.SelectionStart = selectionStart;
+            m_rtb.SelectionLength = selectionLength;
         }
 
         public int CurrentColumn
         {
-            get {return (textBox.SelectionStart - textBox.GetFirstCharIndexOfCurrentLine() + 1);}
+            get { return (textBox.SelectionStart - textBox.GetFirstCharIndexOfCurrentLine() + 1); }
         }
 
         public int CurrentLine
